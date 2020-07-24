e6a56941d803ffc6e6931103db841a0e199e187d
mb0606 <marcoberardini@gmail.com>
2020-07-24 09:16:59 -0700

    Adding overlay to spinner component

--- src/components/AppSpinner.jsx
+++ src/components/AppSpinner.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Spinner } from 'react-bootstrap'
 
+import './AppSpinner.scss'
+
 const AppSpinner = () => {
   return (
-    <div style={{ width: '100%', textAlign: 'center' }}>
-      <Spinner animation="border" role="status">
+    <div id="overlay" style={{ width: '100%', textAlign: 'center' }}>
+      <Spinner
+        className="app-spinner"
+        animation="border"
+        variant="dark"
+        role="status"
+      >
         <span className="sr-only">Loading...</span>
       </Spinner>
     </div>
